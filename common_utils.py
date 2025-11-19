d72103492ab80a7f061fa4de86a520eebedd7c74
AmaySharma06 <amaysharma14102006@gmail.com>
2025-11-19 23:42:33 +0530

    Added basic utils

---
+++ common_utils.py
@@ -0,0 +1,34 @@
+import random
+
+def get_random_graph(n, p=0.5):
+    matrix = [[0] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(i + 1, n):
+            if random.random() < p:
+                matrix[i][j] = 1
+                matrix[j][i] = 1
+    return matrix
+
+def mat_to_list(matrix):
+    adj = {}
+    for i, row in enumerate(matrix):
+        adj[i] = [j for j, val in enumerate(row) if val == 1]
+    return adj
+
+def list_to_mat(adj):
+    n = len(adj)
+    matrix = [[0] * n for _ in range(n)]
+    for node, neighbors in adj.items():
+        for neighbor in neighbors:
+            matrix[node][neighbor] = 1
+    return matrix
+
+# Usage
+if __name__ == "__main__":
+    n = 5
+    matrix = get_random_graph(n)
+    adj_list = mat_to_list(matrix)
+    matrix_back = list_to_mat(adj_list)
+
+    print(matrix)
+    print(adj_list)
